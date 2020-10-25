bb20d0a11321b57ef61d0fc23125342a3ea4f37b
Andrea <andreademasi80@gmail.com>
2020-10-25 18:22:51 +0100

    First implementation of encryption #5

--- src/repositories/history.repository.ts
+++ src/repositories/history.repository.ts
@@ -1,14 +1,15 @@
-import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
-import {History, HistoryRelations, Credential} from '../models';
+import {Getter, inject} from '@loopback/core';
+import {BelongsToAccessor, DefaultCrudRepository, repository} from '@loopback/repository';
 import {DbDataSource} from '../datasources';
-import {inject, Getter} from '@loopback/core';
+import {Encryption} from '../encryption/encryption';
+import {Credential, History, HistoryRelations} from '../models';
 import {CredentialRepository} from './credential.repository';
 
 export class HistoryRepository extends DefaultCrudRepository<
   History,
   typeof History.prototype.id,
   HistoryRelations
-> {
+  > {
 
   public readonly credential: BelongsToAccessor<Credential, typeof History.prototype.id>;
 
@@ -17,5 +18,19 @@ export class HistoryRepository extends DefaultCrudRepository<
   ) {
     super(History, dataSource);
     this.credential = this.createBelongsToAccessorFor('credential', credentialRepositoryGetter,);
+  }
+
+  definePersistedModel(entityClass: typeof History) {
+    const modelClass = super.definePersistedModel(entityClass);
+    const crypt = new Encryption();
+    modelClass.observe('before save', async ctx => {
+      ctx.instance.password = crypt.encrypt(ctx.instance.password);
+      console.log(ctx.instance.password);
+    });
+    modelClass.observe('loaded', async ctx => {
+      console.log(ctx);
+      ctx.data.password = crypt.decrypt(ctx.data.password);
+    })
+    return modelClass;
   }
 }
