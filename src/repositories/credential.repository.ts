e75c5cd58b28fe45f7ea2a18f50cbbce2423c7f5
DobySama <boassi.domenico@gmail.com>
2020-10-26 09:37:47 +0100

    Added a belongsTo relation from Credentials to Service
    Added a belongsTo relation from Credentials to Service through a serviceId

--- src/repositories/credential.repository.ts
+++ src/repositories/credential.repository.ts
@@ -1,8 +1,9 @@
-import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
-import {Credential, CredentialRelations, History} from '../models';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory, BelongsToAccessor} from '@loopback/repository';
+import {Credential, CredentialRelations, History, Service} from '../models';
 import {DbDataSource} from '../datasources';
 import {inject, Getter} from '@loopback/core';
 import {HistoryRepository} from './history.repository';
+import {ServiceRepository} from './service.repository';
 
 export class CredentialRepository extends DefaultCrudRepository<
   Credential,
@@ -12,10 +13,14 @@ export class CredentialRepository extends DefaultCrudRepository<
 
   public readonly histories: HasManyRepositoryFactory<History, typeof Credential.prototype.id>;
 
+  public readonly service: BelongsToAccessor<Service, typeof Credential.prototype.id>;
+
   constructor(
-    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('HistoryRepository') protected historyRepositoryGetter: Getter<HistoryRepository>,
+    @inject('datasources.db') dataSource: DbDataSource, @repository.getter('HistoryRepository') protected historyRepositoryGetter: Getter<HistoryRepository>, @repository.getter('ServiceRepository') protected serviceRepositoryGetter: Getter<ServiceRepository>,
   ) {
     super(Credential, dataSource);
+    this.service = this.createBelongsToAccessorFor('service', serviceRepositoryGetter,);
+    this.registerInclusionResolver('service', this.service.inclusionResolver);
     this.histories = this.createHasManyRepositoryFactoryFor('histories', historyRepositoryGetter,);
     this.registerInclusionResolver('histories', this.histories.inclusionResolver);
   }
